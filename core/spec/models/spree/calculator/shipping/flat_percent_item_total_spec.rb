9058cfd2ff11ea6a71bed153d9dc08601361f20a
swcraig <sebastian@stembolt.com>
2017-08-22 13:51:28 -0700

    flat_percent_item_total_spec

--- core/spec/models/spree/calculator/shipping/flat_percent_item_total_spec.rb
+++ core/spec/models/spree/calculator/shipping/flat_percent_item_total_spec.rb
@@ -4,13 +4,13 @@ require 'shared_examples/calculator_shared_examples'
 module Spree
   module Calculator::Shipping
     describe FlatPercentItemTotal, type: :model do
-      let(:variant1) { build(:variant, price: 10.11) }
-      let(:variant2) { build(:variant, price: 20.2222) }
+      let(:variant1) { build_stubbed(:variant, price: 10.11) }
+      let(:variant2) { build_stubbed(:variant, price: 20.2222) }
 
       it_behaves_like 'a calculator with a description'
 
-      let(:line_item1) { build(:line_item, variant: variant1) }
-      let(:line_item2) { build(:line_item, variant: variant2) }
+      let(:line_item1) { build_stubbed(:line_item, variant: variant1) }
+      let(:line_item2) { build_stubbed(:line_item, variant: variant2) }
 
       let(:package) do
         build(:stock_package, variants_contents: { variant1 => 2, variant2 => 1 })
