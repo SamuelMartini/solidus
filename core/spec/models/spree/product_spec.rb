6d58a02137da91687e74f1ff4bcde86ed2287461
swcraig <sebastian.w.craig@gmail.com>
2017-10-25 15:09:21 -0700

    Add `#gallery` to `Variant` and `Product`
    Image upload extensions will be able to define their own gallery classes
    to plug in here. Using this interface will allow us to remove images
    from Solidus proper, and move them (and all the related
    Paperclip-specific logic) into the solidus_paperclip extension.

--- core/spec/models/spree/product_spec.rb
+++ core/spec/models/spree/product_spec.rb
@@ -522,4 +522,13 @@ RSpec.describe Spree::Product, type: :model do
       end
     end
   end
+
+  describe '#gallery' do
+    let(:product) { Spree::Product.new }
+    subject { product.gallery }
+
+    it 'responds to #images' do
+      expect(subject).to respond_to(:images)
+    end
+  end
 end
