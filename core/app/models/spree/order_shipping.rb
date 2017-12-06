925028e551fac3c276fb850bbd1a5df2e509a350
swcraig <sebastian@stembolt.com>
2017-12-06 14:20:11 -0800

    Use events in `Spree::OrderShipping`

--- core/app/models/spree/order_shipping.rb
+++ core/app/models/spree/order_shipping.rb
@@ -71,17 +71,12 @@ class Spree::OrderShipping
       shipment.update_columns(state: 'shipped', shipped_at: Time.current)
     end
 
-    send_shipment_emails(carton) if stock_location.fulfillable? && !suppress_mailer # e.g. digital gift cards that aren't actually shipped
+    Spree.event_bus.publish(
+      :carton_shipped,
+      Spree::Events::CartonShippedEvent.new(carton: carton, suppress_mailer: suppress_mailer)
+    )
     @order.recalculate
 
     carton
-  end
-
-  private
-
-  def send_shipment_emails(carton)
-    carton.orders.each do |order|
-      Spree::Config.carton_shipped_email_class.shipped_email(order: order, carton: carton).deliver_later
-    end
   end
 end
