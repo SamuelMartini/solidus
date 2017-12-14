33e1920b3f83bfcccaabd985de73c08d98d5d430
swcraig <sebastian@stembolt.com>
2017-12-14 14:41:50 -0800

    more hack for zone_member

--- core/app/models/spree/zone_member.rb
+++ core/app/models/spree/zone_member.rb
@@ -3,7 +3,7 @@ module Spree
     belongs_to :zone, class_name: 'Spree::Zone', counter_cache: true, inverse_of: :zone_members
     serialize :zoneable, Spree::SerializeCarmen
 
-    def initialize(zoneable:)
+    def initialize(zoneable:, zone: nil)
       super
       type = (zoneable.class == Carmen::Country) ? 'Spree::Country' : 'Spree::State'
       update_attributes(zoneable_type: type)
