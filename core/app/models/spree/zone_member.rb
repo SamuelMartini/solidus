ffebd45c68648e6861e7d55e83df98b71ad025f7
swcraig <sebastian@stembolt.com>
2017-12-14 14:36:01 -0800

    Use Carmen in ZoneMember
    This is pretty ugly.

--- core/app/models/spree/zone_member.rb
+++ core/app/models/spree/zone_member.rb
@@ -1,7 +1,13 @@
 module Spree
   class ZoneMember < Spree::Base
     belongs_to :zone, class_name: 'Spree::Zone', counter_cache: true, inverse_of: :zone_members
-    belongs_to :zoneable, polymorphic: true
+    serialize :zoneable, Spree::SerializeCarmen
+
+    def initialize(zoneable:)
+      super
+      type = (zoneable.class == Carmen::Country) ? 'Spree::Country' : 'Spree::State'
+      update_attributes(zoneable_type: type)
+    end
 
     delegate :name, to: :zoneable, allow_nil: true
   end
