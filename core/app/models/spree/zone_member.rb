5cb220fa489f096ef26dcd09c9b245d2db3cfcc0
swcraig <sebastian@stembolt.com>
2017-12-15 19:15:25 -0800

    Spree::Zone works

--- core/app/models/spree/zone_member.rb
+++ core/app/models/spree/zone_member.rb
@@ -1,7 +1,25 @@
 module Spree
   class ZoneMember < Spree::Base
     belongs_to :zone, class_name: 'Spree::Zone', counter_cache: true, inverse_of: :zone_members
-    belongs_to :zoneable, polymorphic: true
+    serialize :zoneable, Spree::SerializeCarmen
+
+    before_save :set_type
+
+    def set_type
+      type = (zoneable.class == Carmen::Country) ? 'Spree::Country' : 'Spree::State'
+      self[:zoneable_type] = type
+    end
+    # def zoneable=(thing)
+    #   self[:zoneable] = thing
+    # end
+
+    # def initialize(zoneable:, zone: nil)
+    #   super
+    #   require 'pry'
+    #   binding.pry
+    #   type = (zoneable.class == Carmen::Country) ? 'Spree::Country' : 'Spree::State'
+    #   update_attributes(zoneable: zoneable, zoneable_type: type)
+    # end
 
     delegate :name, to: :zoneable, allow_nil: true
   end
